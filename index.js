0c38342c170ec30f68f7abeda762e843f82174d3
kamil <akorede.kamil@gmail.com>
2023-07-27 18:57:10 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// navbar toggle
+const navbarShowBtn = document.querySelector('.navbar-show-btn');
+const navbarCollapseDiv = document.querySelector('.navbar-collapse');
+const navbarHideBtn = document.querySelector('.navbar-hide-btn');
+
+navbarShowBtn.addEventListener('click', function(){
+    navbarCollapseDiv.classList.add('navbar-show');
+});
+navbarHideBtn.addEventListener('click', function(){
+    navbarCollapseDiv.classList.remove('navbar-show');
+});
+
+// changing search icon image
+window.addEventListener('resize', changeSearchIcon);
+function changeSearchIcon(){
+    let winSize = window.matchMedia("(min-width: 1200px)");
+    if(winSize.matches){
+        document.querySelector('.search-icon img').src = "images/search-icon.png";
+    } else {
+        document.querySelector('.search-icon img').src = "images/search-icon-dark.png";
+    }
+}
+
+changeSearchIcon();
